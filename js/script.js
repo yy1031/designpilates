4a0e0bebdc118568f43a50d5d860aff8cff3521f
yukako <tomtomko.0310@gmail.com>
2025-03-28 17:48:50 +0900

    wow.jsの追加

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
-$(".header-nav-bars").click(function(){
-   $(".header-logo").toggleClass("open");
-   $(this).toggleClass("open");
-   $(".header-btn").toggleClass("open");
-   $(".header-nav-wrap").toggleClass("open");
-})
+$(function(){
+  new WOW().init();
 
-$(".faq-item-plus").click(function(){
-   $(this).parent().next(".faq-item-a").slideToggle();
-   $(this).toggleClass("open");
-})
+  $(".header-nav-bars").click(function(){
+     $(".header-logo").toggleClass("open");
+     $(this).toggleClass("open");
+     $(".header-btn").toggleClass("open");
+     $(".header-nav-wrap").toggleClass("open");
+  })
+  
+  $(".faq-item-plus").click(function(){
+     $(this).parent().next(".faq-item-a").slideToggle();
+     $(this).toggleClass("open");
+  })
+  
+  $(window).scroll(function () {
+    var main = $(".main").innerHeight();
+     if($(window).scrollTop() > main) {
+       $('.header-logo').addClass('scrolled');
+       $('.header-btn').addClass('scrolled');
+       $('.header-nav-bars').addClass('scrolled');
+     } else {
+       $('.header-logo').removeClass('scrolled');
+       $('.header-btn').removeClass('scrolled');
+       $('.header-nav-bars').removeClass('scrolled');
+     }
+   });
 
-$(window).scroll(function () {
-  var main = $(".main").innerHeight();
-   if($(window).scrollTop() > main) {
-     $('.header-logo').addClass('scrolled');
-     $('.header-btn').addClass('scrolled');
-     $('.header-nav-bars').addClass('scrolled');
-   } else {
-     $('.header-logo').removeClass('scrolled');
-     $('.header-btn').removeClass('scrolled');
-     $('.header-nav-bars').removeClass('scrolled');
-   }
- });
+})
